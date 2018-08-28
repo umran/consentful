62cce549f1b6123079b26010268609c8e7687261
Umran Hussein <umran.hussein@gmail.com>
2018-08-27 22:02:46 -0700

    use proper json extension when requiring

--- src/protocol/schemas/index.js
+++ src/protocol/schemas/index.js
@@ -1,9 +1,9 @@
-const invoice = require('./invoice')
-const invoiceReceipt = require('./invoiceReceipt')
-const promiseOfPayment = require('./promiseOfPayment')
-const paymentRequest = require('./paymentRequest')
-const escrowContract = require('./escrowContract')
-const proofOfDelivery = require('./proofOfDelivery')
+const invoice = require('./invoice.json')
+const invoiceReceipt = require('./invoiceReceipt.json')
+const promiseOfPayment = require('./promiseOfPayment.json')
+const paymentRequest = require('./paymentRequest.json')
+const escrowContract = require('./escrowContract.json')
+const proofOfDelivery = require('./proofOfDelivery.json')
 
 exports.invoice = invoice
 exports.invoiceReceipt = invoiceReceipt
